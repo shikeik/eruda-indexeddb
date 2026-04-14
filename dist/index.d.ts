448a289636c3b3a85a209266781895c02262cf52
shikeik <2667097598@qq.com>
2026-04-15 05:00:01 +0800

    init: fork eruda-indexeddb with enhanced native UI

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export declare function mountIndexedDB(erudaObj: typeof import('eruda')): {
+    destroy: () => void;
+};
+export default mountIndexedDB;
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/utils.d.ts
@@ -0,0 +1,2 @@
+export declare function classPrefix(str: string): string;
+//# sourceMappingURL=utils.d.ts.map
