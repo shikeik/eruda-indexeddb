eedb768c352f8d5c1855df24b0955df72f50423d
shikeik <2667097598@qq.com>
2026-04-15 05:00:11 +0800

    chore: 忽略 node_modules 和 dist

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function mountIndexedDB(erudaObj: typeof import('eruda')): {
-    destroy: () => void;
-};
-export default mountIndexedDB;
-//# sourceMappingURL=index.d.ts.map
--- dist/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function classPrefix(str: string): string;
-//# sourceMappingURL=utils.d.ts.map
